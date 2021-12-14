3e154aec9737de145603061faf698ea8c0054832
Peta <peremitinat@mail.ru>
2021-12-14 13:50:59 +0700

    init commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTo8
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Стартовая функция главного класса программы
+        {
+            //Вывод приглашения на экран
+            Console.WriteLine("Введите двоичное чиcло: ");
+            //Ввод строки, конвертация ее из указанной СС в 10-тичную
+            int i = Convert.ToInt32(Console.ReadLine(), 2);
+            //Сконвертированное в 10-тичную переконвертируется в 8-ричную
+            string s = Convert.ToString(i, 8);
+            //Вывод результата
+            Console.WriteLine("В восьмеричной системе счисления это: " + s);
+            //Просим консоль не закрываться сразу.
+            Console.ReadLine();
+
+        }
+    }
+}
